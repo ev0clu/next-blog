b9b6ae69e9aad23fa28c795e593507b71dccfde7
ev0clu <ev0clu@gmail.com>
2023-10-26 15:37:25 +0200

    Register API is created

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,67 @@
+import { NextResponse } from 'next/server';
+import { hash } from 'bcrypt';
+import prisma from '@/lib/db';
+
+export async function POST(request: Request) {
+  try {
+    const { username, email, password } = await request.json();
+
+    const existUserByName = await prisma.user.findUnique({
+      where: {
+        username: username
+      }
+    });
+
+    if (existUserByName) {
+      return NextResponse.json(
+        {
+          user: null,
+          message: 'Username already exist'
+        },
+        {
+          status: 409
+        }
+      );
+    }
+
+    const existUserByEmail = await prisma.user.findUnique({
+      where: {
+        email: email
+      }
+    });
+
+    if (existUserByEmail) {
+      return NextResponse.json(
+        {
+          user: null,
+          message: 'Email already exist'
+        },
+        {
+          status: 409
+        }
+      );
+    }
+
+    const hashedPassword = await hash(password, 10);
+
+    const newUser = await prisma.user.create({
+      data: {
+        username: username,
+        email: email,
+        password: hashedPassword
+      }
+    });
+
+    //const { password: newUserPassword, ...rest } = newUser;
+
+    return NextResponse.json(
+      {
+        //user: rest,
+        message: 'User created'
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
