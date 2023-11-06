b94a2675d21108bddaa1f4a46c09ec4f2b29981e
ev0clu <ev0clu@gmail.com>
2023-11-06 16:55:03 +0100

    Root page is updated

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,6 @@ export default async function Home() {
     <main className="flex flex-1 flex-col items-center">
       <div className="min-w-350 m-5 grid grid-cols-2 gap-4 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
         <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
       </div>
     </main>
   );
