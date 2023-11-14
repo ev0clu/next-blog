f7f027796d6d1e4f708570c60cac2430c635b98a
ev0clu <ev0clu@gmail.com>
2023-11-14 12:18:24 +0100

    Loading component is renamed

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,11 +1,11 @@
-const LoadingPage = () => {
+const Loading = () => {
   return (
     <div className="loader">
       <svg
-        className="animate-spin h-5 w-5 mr-3"
+        className="mr-3 h-5 w-5 animate-spin"
         viewBox="0 0 24 24"
       ></svg>
     </div>
   );
 };
-export default LoadingPage;
+export default Loading;
