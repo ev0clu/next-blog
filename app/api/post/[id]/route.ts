6670c267bd36b73eb89ef316b630430677f4a15d
ev0clu <ev0clu@gmail.com>
2023-11-14 16:06:45 +0100

    Prisma update query handling is fix with moving the content to PUT request

--- app/api/post/[id]/route.ts
+++ app/api/post/[id]/route.ts
@@ -6,15 +6,6 @@ export async function GET(
   { params }: { params: { id: string } }
 ) {
   try {
-    const updatePostViews = await prisma.post.update({
-      where: { id: params.id },
-      data: {
-        views: {
-          increment: 1
-        }
-      }
-    });
-
     const post = await prisma.post.findUnique({
       where: { id: params.id },
       include: {
@@ -31,6 +22,37 @@ export async function GET(
       {
         post: post,
         message: `Post id:${params.id} records are returned with comments`
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: error
+      },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PUT(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const updatePostViews = await prisma.post.update({
+      where: { id: params.id },
+      data: {
+        views: {
+          increment: 1
+        }
+      }
+    });
+
+    return NextResponse.json(
+      {
+        post: updatePostViews,
+        message: `Post id:${params.id} views record is updated`
       },
       { status: 201 }
     );
