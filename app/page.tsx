934584cafccb619657e53b6509837e455e861a9e
ev0clu <ev0clu@gmail.com>
2023-11-15 15:38:27 +0100

    Home page rendering method is updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import PostCard from '@/components/PostCard';
 
 export default async function Home() {
-  return (
-    <div className="min-w-350 m-5 grid grid-cols-2 gap-4 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
-      <PostCard />
-    </div>
-  );
+  return <PostCard />;
 }
