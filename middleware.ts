aff3a3ceb7df55fcfe4d65cb88a0b208d11a88c5
ev0clu <ev0clu@gmail.com>
2023-11-17 16:03:42 +0100

    Middleware macher is updated

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 export { default } from 'next-auth/middleware';
 
 export const config = {
-  matcher: ['/profile', '/admin', '/post/new']
+  matcher: ['/profile', '/admin', '/post/new', '/post/:path*/update']
 };
