c5f8fa7f70f2f11c214ce2e88862db3a68fd60af
ev0clu <ev0clu@gmail.com>
2023-11-17 15:48:37 +0100

    Post delete handling with modal window and UI is added to post page

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -15,6 +15,8 @@ import PostComments from '@/components/PostComments';
 import ErrorMessage from '@/components/ErrorMessage';
 import getNumberOfComments from '@/lib/getNumberOfComments';
 import Loading from '@/components/Loading';
+import { useRouter } from 'next/navigation';
+import Spinner from '@/components/Spinner';
 
 const Post = ({
   params
@@ -28,6 +30,10 @@ const Post = ({
   const { data: session } = useSession();
   const { id } = params;
   const [isLoading, setIsLoading] = useState(true);
+  const [deletePopup, setDeletePopup] = useState(false);
+  const [isSubmitting, setSubmitting] = useState(false);
+
+  const router = useRouter();
 
   useEffect(() => {
     const fetchDataUpdate = async () => {
@@ -85,6 +91,36 @@ const Post = ({
     setIsNewComment(true);
   };
 
+  const handleDeletePopup = () => {
+    setDeletePopup((prevValue) => !prevValue);
+  };
+
+  const handleDeleteClick = async () => {
+    try {
+      setError('');
+      setSubmitting(true);
+      const response = await fetch(`/api/post/${id}/delete`, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' }
+      });
+      if (response.ok) {
+        router.push('/');
+      } else {
+        const body = await response.json();
+        handleDeletePopup();
+        setSubmitting(false);
+        if (body.message) {
+          setError(body.message);
+        } else {
+          setError('An unexpected error occurred');
+        }
+      }
+    } catch (error) {
+      setError('An unexpected error is occured');
+      setSubmitting(false);
+    }
+  };
+
   if (isLoading) {
     return <Loading />;
   }
@@ -93,93 +129,126 @@ const Post = ({
     <>
       {error && <ErrorMessage>{error}</ErrorMessage>}
       {post ? (
-        <div className="flex flex-col gap-2">
+        <>
           <div
             className={`${
-              theme === 'light'
-                ? 'border-neutral-300'
-                : 'border-neutral-700'
-            } flex flex-col justify-between rounded-lg border`}
-            key={post.id}
+              deletePopup ? 'pointer-events-none' : ''
+            } flex flex-col gap-2`}
           >
-            <div className="flex flex-col gap-2 px-5 pt-5">
-              <div className="opacity-50">
-                {format(
-                  new Date(post.createdAt),
-                  'hh:mmaaa MMM do, yyyy'
-                )}{' '}
-                by {post.author.username}
-              </div>
-              <Link href="/post/new" className="text-lg font-bold">
-                {post.title}
-              </Link>
-              <Markdown
-                className={`prose prose-code:text-blue-300 ${
-                  theme == 'light'
-                    ? 'text-slate-900 prose-headings:text-slate-900 prose-p:text-slate-900 prose-blockquote:text-slate-900 prose-strong:text-slate-900 prose-ol:text-slate-900 prose-ul:text-slate-900 prose-li:text-slate-900'
-                    : 'text-slate-100 prose-headings:text-slate-100 prose-p:text-slate-100 prose-blockquote:text-slate-100 prose-strong:text-slate-100 prose-ol:text-slate-100 prose-ul:text-slate-100 prose-li:text-slate-100'
-                }`}
-              >
-                {post.description}
-              </Markdown>
-              <Markdown
-                className={`prose prose-code:text-blue-300 ${
-                  theme == 'light'
-                    ? 'text-slate-900 prose-headings:text-slate-900 prose-p:text-slate-900 prose-blockquote:text-slate-900 prose-strong:text-slate-900 prose-ol:text-slate-900 prose-ul:text-slate-900 prose-li:text-slate-900'
-                    : 'text-slate-100 prose-headings:text-slate-100 prose-p:text-slate-100 prose-blockquote:text-slate-100 prose-strong:text-slate-100 prose-ol:text-slate-100 prose-ul:text-slate-100 prose-li:text-slate-100'
-                }`}
-              >
-                {post.content}
-              </Markdown>
-            </div>
             <div
               className={`${
-                session?.user && session?.user.role === 'ADMIN'
-                  ? 'justify-between'
-                  : 'justify-end'
-              } flex flex-row items-center gap-4 px-5 py-3`}
+                theme === 'light'
+                  ? 'border-neutral-300'
+                  : 'border-neutral-700'
+              } flex flex-col justify-between rounded-lg border`}
+              key={post.id}
             >
-              {session?.user && session?.user.role === 'ADMIN' && (
-                <div className="flex flex-row items-center gap-2">
-                  <Link
-                    href={`/post/${id}/delete`}
-                    className="hover:opacity-70"
-                  >
-                    <MdDelete />
-                  </Link>
-                  <Link
-                    href={`/post/${id}/update`}
-                    className="hover:opacity-70"
-                  >
-                    <MdEdit />
-                  </Link>
+              <div className="flex flex-col gap-2 px-5 pt-5">
+                <div className="opacity-50">
+                  {format(
+                    new Date(post.createdAt),
+                    'hh:mmaaa MMM do, yyyy'
+                  )}{' '}
+                  by {post.author.username}
                 </div>
-              )}
-              <div className="flex flex-row items-center gap-1">
-                <div className="flex flex-row items-center gap-1 opacity-70">
-                  <BiMessageRounded />
-                  {getNumberOfComments(post.comments)}
-                </div>
-                <div className="flex flex-row items-center gap-1 opacity-70">
-                  <AiOutlineEye />
-                  {post.views}
+                <Link href="/post/new" className="text-lg font-bold">
+                  {post.title}
+                </Link>
+                <Markdown
+                  className={`prose prose-code:text-blue-300 ${
+                    theme == 'light'
+                      ? 'text-slate-900 prose-headings:text-slate-900 prose-p:text-slate-900 prose-blockquote:text-slate-900 prose-strong:text-slate-900 prose-ol:text-slate-900 prose-ul:text-slate-900 prose-li:text-slate-900'
+                      : 'text-slate-100 prose-headings:text-slate-100 prose-p:text-slate-100 prose-blockquote:text-slate-100 prose-strong:text-slate-100 prose-ol:text-slate-100 prose-ul:text-slate-100 prose-li:text-slate-100'
+                  }`}
+                >
+                  {post.description}
+                </Markdown>
+                <Markdown
+                  className={`prose prose-code:text-blue-300 ${
+                    theme == 'light'
+                      ? 'text-slate-900 prose-headings:text-slate-900 prose-p:text-slate-900 prose-blockquote:text-slate-900 prose-strong:text-slate-900 prose-ol:text-slate-900 prose-ul:text-slate-900 prose-li:text-slate-900'
+                      : 'text-slate-100 prose-headings:text-slate-100 prose-p:text-slate-100 prose-blockquote:text-slate-100 prose-strong:text-slate-100 prose-ol:text-slate-100 prose-ul:text-slate-100 prose-li:text-slate-100'
+                  }`}
+                >
+                  {post.content}
+                </Markdown>
+              </div>
+              <div
+                className={`${
+                  session?.user && session?.user.role === 'ADMIN'
+                    ? 'justify-between'
+                    : 'justify-end'
+                } flex flex-row items-center gap-4 px-5 py-3`}
+              >
+                {session?.user && session?.user.role === 'ADMIN' && (
+                  <div className="flex flex-row items-center gap-2">
+                    <button
+                      className="hover:opacity-70"
+                      onClick={handleDeletePopup}
+                    >
+                      <MdDelete />
+                    </button>
+                    <Link
+                      href={`/post/${id}/update`}
+                      className="hover:opacity-70"
+                    >
+                      <MdEdit />
+                    </Link>
+                  </div>
+                )}
+                <div className="flex flex-row items-center gap-1">
+                  <div className="flex flex-row items-center gap-1 opacity-70">
+                    <BiMessageRounded />
+                    {getNumberOfComments(post.comments)}
+                  </div>
+                  <div className="flex flex-row items-center gap-1 opacity-70">
+                    <AiOutlineEye />
+                    {post.views}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          {session?.user && (
+            {session?.user && (
+              <div>
+                <NewComment
+                  postId={params.id}
+                  handleNewCommentClick={handleNewCommentClick}
+                />
+              </div>
+            )}
             <div>
-              <NewComment
-                postId={params.id}
-                handleNewCommentClick={handleNewCommentClick}
-              />
+              <p className="text-xl font-bold underline">Comments</p>
+              <PostComments comments={post.comments} />
+            </div>
+          </div>
+          {deletePopup && (
+            <div className="absolute left-0 top-0 flex h-full w-screen items-center justify-center bg-zinc-600 bg-opacity-70">
+              <div className="w-80 rounded-lg bg-white p-5 text-slate-900">
+                <p>
+                  {`Are you sure you want to delete this post? You can't
+                undo this.`}
+                </p>
+                <div className="mt-3 flex flex-row justify-center gap-10">
+                  <button
+                    className="rounded-full bg-slate-200 px-4 py-2 text-slate-900"
+                    type="button"
+                    onClick={handleDeletePopup}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    className="flex flex-row items-center gap-2 rounded-full bg-red-500 px-4 py-2 text-slate-200"
+                    type="submit"
+                    onClick={handleDeleteClick}
+                    disabled={isSubmitting}
+                  >
+                    Delete{isSubmitting && <Spinner />}
+                  </button>
+                </div>
+              </div>
             </div>
           )}
-          <div>
-            <p className="text-xl font-bold underline">Comments</p>
-            <PostComments comments={post.comments} />
-          </div>
-        </div>
+        </>
       ) : (
         <div className="my-10 flex flex-1 flex-col items-center justify-center gap-2 text-center">
           <p className="text-xl font-bold">
