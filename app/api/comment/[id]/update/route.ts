e47ca89b70842926b63cc801a743ea92c40e35a9
ev0clu <ev0clu@gmail.com>
2023-11-19 11:43:01 +0100

    Comment edit component and API is created

---
+++ app/api/comment/[id]/update/route.ts
@@ -0,0 +1,43 @@
+import { NextResponse } from 'next/server';
+import prisma from '@/prisma/client';
+import { z } from 'zod';
+
+const commentSchema = z.object({
+  comment: z.string().min(1, 'Comment is required')
+});
+
+export async function PUT(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const body = await req.json();
+
+    // Validation with safeParse
+    const validation = commentSchema.safeParse(body);
+    if (!validation.success) {
+      NextResponse.json(validation.error.format(), { status: 400 });
+    }
+    const updateComment = await prisma.comment.update({
+      where: { id: params.id },
+      data: {
+        content: body.comment
+      }
+    });
+
+    return NextResponse.json(
+      {
+        comment: updateComment,
+        message: `Comment id:${params.id} is updated`
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: error
+      },
+      { status: 500 }
+    );
+  }
+}
---
+++ components/EditComment.tsx
@@ -0,0 +1,136 @@
+'use client';
+
+import Spinner from './Spinner';
+import ErrorMessage from './ErrorMessage';
+import { ThemeContext } from '@/context/ThemeContext';
+import { useContext, useState } from 'react';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useSession } from 'next-auth/react';
+
+const formSchema = z.object({
+  comment: z.string().min(1, 'Comment is required')
+});
+
+type formType = z.infer<typeof formSchema>;
+
+interface Props {
+  commentId: string;
+  commentContent: string;
+  handleCommentRefreshClick: () => void;
+  handleCancelCommentUpdateClick: () => void;
+}
+
+const EditComment = ({
+  commentId,
+  commentContent,
+  handleCommentRefreshClick,
+  handleCancelCommentUpdateClick
+}: Props) => {
+  const { theme } = useContext(ThemeContext);
+  const [error, setError] = useState('');
+  const [isSubmitting, setSubmitting] = useState(false);
+  const { data: session } = useSession();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<formType>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      comment: commentContent
+    }
+  });
+
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    try {
+      setError('');
+      setSubmitting(true);
+      const response = await fetch(
+        `/api/comment/${commentId}/update`,
+        {
+          method: 'PUT',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            comment: data.comment
+          })
+        }
+      );
+
+      if (response.ok) {
+        setError('');
+        setSubmitting(false);
+        handleCommentRefreshClick();
+        handleCancelCommentUpdateClick();
+      } else {
+        const body = await response.json();
+        if (body.message) {
+          setError(body.message);
+        } else {
+          setError('An unexpected error occurred');
+        }
+      }
+    } catch (error) {
+      setError('An unexpected error is occured');
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <>
+      {error && <ErrorMessage>{error}</ErrorMessage>}
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="mt-5 flex flex-col gap-3"
+      >
+        <textarea
+          id="comment"
+          autoComplete="comment"
+          className="resize-none rounded px-2 py-1 text-slate-950"
+          rows={3}
+          placeholder="Type comment..."
+          {...register('comment')}
+        />
+        {errors && (
+          <ErrorMessage>{errors.comment?.message}</ErrorMessage>
+        )}
+        <div className="mt-2 flex flex-row items-center justify-center gap-4">
+          <button
+            className={`${
+              theme === 'light'
+                ? !isSubmitting
+                  ? 'bg-slate-950 text-slate-100'
+                  : 'bg-neutral-400 text-slate-950'
+                : !isSubmitting
+                ? 'bg-slate-100 text-slate-950'
+                : 'bg-neutral-400 text-slate-950'
+            } flex flex-row items-center justify-center gap-2 rounded p-2`}
+            type="button"
+            onClick={handleCancelCommentUpdateClick}
+          >
+            Cancel
+          </button>
+          <button
+            className={`${
+              theme === 'light'
+                ? !isSubmitting
+                  ? 'bg-slate-950 text-slate-100'
+                  : 'bg-neutral-400 text-slate-950'
+                : !isSubmitting
+                ? 'bg-slate-100 text-slate-950'
+                : 'bg-neutral-400 text-slate-950'
+            } flex flex-row items-center justify-center gap-2 rounded p-2`}
+            type="submit"
+            disabled={isSubmitting}
+          >
+            Send {isSubmitting && <Spinner />}
+          </button>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default EditComment;
